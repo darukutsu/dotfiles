00a5259e70299446fdec8098b9f2078d17ef8f25
darukutsu <darupeter@pm.me>
2025-04-16 12:32:08 +0200

    add universal dropdown ability marked windows can be bind to f20-f24 they will behave as dropdown terminal/scratchpad

--- sxhkd/dropdown.sh
+++ sxhkd/dropdown.sh
@@ -4,8 +4,27 @@
 dropname="$1"
 wid=$(xdotool search --classname "$dropname")
 
-if [ -z "$wid" ]; then
-  kitty --hold --class="$dropname" "$XDG_CONFIG_HOME/sxhkd/unmap_kitty.sh" "$dropname"
+if [ -s "/tmp/$dropname" ]; then
+  wid=$(head -n1 "/tmp/$dropname")
+
+  # removes file if window was destroyed
+  # or when node was marked (thus normal state)
+  if ! bspc query -N -n $wid || { bspc query -N -n $wid.marked && bspc node $wid -g marked=off; }; then
+    rm "/tmp/$dropname"
+    exit
+  fi
+fi
+
+if [ -z "$wid" ] && echo "$dropname" | grep kitty; then
+  kitty --hold --class="$dropname" "$XDG_CONFIG_HOME/sxhkd/unmap.sh" "$dropname"
+elif [ -z "$wid" ]; then
+  # currently only focused window is supported
+  # maybe later will figure out how to send all marked to 1 node
+  # and then operate over multiple marked nodes
+  touch "/tmp/$dropname"
+  # FIX: we cannot directly determine which node is marked
+  bspc query -N -n focused.marked | tail -n1 >"/tmp/$dropname"
+  bspc node "$(head -n1)" -g marked=off
 else
   if bspc node "$wid".!hidden -g hidden=on; then
     :
