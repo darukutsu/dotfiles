a2a0d0fdc043991e2eda48a106d3e2b28fef1467
darukutsu <darupeter@pm.me>
2025-07-30 13:51:00 +0200

    update zmkbat script

--- polybar/scripts/zmkbat.sh
+++ polybar/scripts/zmkbat.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
+#set -x
 
 # More info on
 # https://ukbaz.github.io//howto/python_gio_1.html
 # https://gist.github.com/madushan1000/9744eb6350a5dd9685fb6bfbb25fbb8a
 
 MAC_ADDR=D0_AD_70_06_3E_45
-DBUS_PATH=/org/bluez/hci2
+DBUS_PATH=$(bluetoothctl list | grep -o '/org/bluez/hci[0-9]*')
 L_PATH=service0010/char0011
 R_PATH=service0015/char0016
 
 # do this only to determine path although default should be good
 # NOTE: for some reason it writes 0 every time unless this function runs and unlocks something
 #       so recommend to run this script once every hour
-for path in $(busctl tree org.bluez | grep -o "/org/bluez/hci2/dev_$MAC_ADDR/service.*/char.*"); do
+for path in $(busctl tree org.bluez | grep -o "$DBUS_PATH/dev_$MAC_ADDR/service.*/char.*"); do
   busctl get-property org.bluez "$path" org.bluez.GattCharacteristic1 UUID >/dev/null 2>&1 &&
     busctl call org.bluez "$path" org.bluez.GattCharacteristic1 ReadValue a{sv} 0 >/dev/null 2>&1
   #echo $path
