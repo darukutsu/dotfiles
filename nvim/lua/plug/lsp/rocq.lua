19e5f00c5c122b622fc546a0bfd208fe60355b3d
darukutsu <darupeter@pm.me>
2025-11-30 16:35:19 +0100

    typo

--- nvim/lua/plug/lsp/rocq.lua
+++ nvim/lua/plug/lsp/rocq.lua
@@ -6,35 +6,25 @@ vim.cmd([[
 ]])
 
 require("vsrocq").setup({
-	lsp = {
-		on_attach = function(client, bufnr)
-			local map = vim.keymap.set
-			map(
-				{ "n", "x", "o" },
-				"<leader>rn",
-				":VsRock stepForward<cr>",
-				{ buffer = bufnr, desc = "Roqc step forward" }
-			)
-			map(
-				{ "n", "x", "o" },
-				"<leader>rp",
-				":VsRock stepBackward<cr>",
-				{ buffer = bufnr, desc = "Roqc step backward" }
-			)
-			map(
-				{ "n", "x", "o" },
-				"<leader>rb",
-				":VsRock interpretToPoint<cr>",
-				{ buffer = bufnr, desc = "Roqc interpretToPoint" }
-			)
-			map(
-				{ "n", "x", "o" },
-				"<leader>re",
-				":VsRock interpretToEnd<cr>",
-				{ buffer = bufnr, desc = "Roqc interpretToEnd" }
-			)
-		end,
-		--cmd = { "vsrocqtop" },
-		filetypes = { "coq", "v" },
-	},
+  lsp = {
+    on_attach = function(client, bufnr)
+      local map = vim.keymap.set
+      map({ "n", "x", "o" }, "<leader>rn", ":VsRocq stepForward<cr>", { buffer = bufnr, desc = "Roqc step forward" })
+      map({ "n", "x", "o" }, "<leader>rp", ":VsRocq stepBackward<cr>", { buffer = bufnr, desc = "Roqc step backward" })
+      map(
+        { "n", "x", "o" },
+        "<leader>rb",
+        ":VsRocq interpretToPoint<cr>",
+        { buffer = bufnr, desc = "Roqc interpretToPoint" }
+      )
+      map(
+        { "n", "x", "o" },
+        "<leader>re",
+        ":VsRocq interpretToEnd<cr>",
+        { buffer = bufnr, desc = "Roqc interpretToEnd" }
+      )
+    end,
+    --cmd = { "vsrocqtop" },
+    filetypes = { "coq", "v" },
+  },
 })
