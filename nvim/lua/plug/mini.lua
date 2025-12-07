282159fcb8876c1d871c6d80cff8b01728620f8d
darukutsu <darupeter@pm.me>
2025-12-07 12:07:05 +0100

    handle surround conflict with ' (register jumping)

--- nvim/lua/plug/mini.lua
+++ nvim/lua/plug/mini.lua
@@ -8,12 +8,12 @@
 
 require("mini.surround").setup({
   mappings = {
-    add = ">a", -- Add surrounding in Normal and Visual modes
-    delete = ">d", -- Delete surrounding
-    find = ">f", -- Find surrounding (to the right)
-    find_left = ">F", -- Find surrounding (to the left)
-    highlight = ">h", -- Highlight surrounding
-    replace = ">r", -- Replace surrounding
+    add = "#a", -- Add surrounding in Normal and Visual modes
+    delete = "#d", -- Delete surrounding
+    find = "#f", -- Find surrounding (to the right)
+    find_left = "#F", -- Find surrounding (to the left)
+    highlight = "#h", -- Highlight surrounding
+    replace = "#r", -- Replace surrounding
 
     suffix_last = "l", -- Suffix to search with "prev" method
     suffix_next = "n", -- Suffix to search with "next" method
