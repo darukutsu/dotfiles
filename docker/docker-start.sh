5a1e163581d872ea685f346d45e699fe74e32fa8
darukutsu <darupeter@pm.me>
2026-06-25 16:21:42 +0200

    feat(docker): update updating container script

--- docker/docker-start.sh
+++ docker/docker-start.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -x
 
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  printf "USAGE: docker-start.sh <COMMAND>
+  COMMANDS:
+    update-all 		              updates all container and runs them
+    update-all-running 		      updates all running container and runs them
+    <CONTAINER>	                starts container
+    <CONTAINER> update 		      updates container
+    <CONTAINER> update-force 		force updates container (stopped ones)
+  "
+  exit 0
+fi
+
 #if ! zpool status docker >/dev/null; then
 #  printf "ZFS: docker pool not mounted"
 #  exit
@@ -9,18 +21,42 @@ set -x
 # Use docker compose instead
 root=/home/daru/.docker
 path="$root/$1/docker-compose.yml"
-if [ "$2" = "all" ]; then
-  find "$root" -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | xargs -I{} "$root"/docker-start.sh {} update
-elif [ -n "$2" ]; then
+
+update_container() {
+  # NOTE: updates and runs container
+
   while ! ping -c1 8.8.8.8; do :; done
-  if docker ps | grep -i "$1" || [ "$3" = "force" ]; then # don't update stopped containers
-    #docker stop "$1"
-    docker-compose -f "$path" down
-    # destructive operation be sure you have backup of your data
-    #docker rm -f $()
-    docker-compose -f "$path" pull && docker-compose -f "$path" up -d && docker image prune -af
-    #docker-compose -f "$path" pull && docker image prune -af && docker-compose -f "$path" up -d
+  docker-compose -f "$path" down
+  # destructive operation be sure you have backup of your data
+  docker-compose -f "$path" pull && docker-compose -f "$path" up -d && docker image prune -af
+  #docker-compose -f "$path" pull && docker image prune -af && docker-compose -f "$path" up -d
+}
+
+case "$2" in
+update-all)
+  find "$root" -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | xargs -I{} "$root"/docker-start.sh {} update-force
+  ;;
+update-all-running)
+  find "$root" -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | xargs -I{} "$root"/docker-start.sh {} update
+  ;;
+update)
+  #if docker ps | grep -i "$1" ||
+  state=$(systemctl --user show --property=ActiveState --no-pager --value "$1")
+  if [ "$state" = "active" ]; then
+    update_container
   fi
-else
+  ;;
+update-force)
+  update_container
+  ;;
+  #update-force-stop)
+  #  update_container
+  #;;
+*)
+  echo "unknown option $2"
+  ;;
+esac
+
+if [ -z "$2" ]; then
   docker-compose -f "$path" up
 fi
